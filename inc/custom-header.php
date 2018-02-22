49a6bdd4aa77c3d50a90e07c0a08501cd7e4d801
Thomas Sweeney <skywriter@gmail.com>
2018-02-22 19:10:12 +0000

    Added new header

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -18,10 +18,11 @@
  */
 function literature_city_custom_header_setup() {
 	add_theme_support( 'custom-header', apply_filters( 'literature_city_custom_header_args', array(
-		'default-image'          => '',
-		'default-text-color'     => '000000',
-		'width'                  => 1000,
-		'height'                 => 250,
+		'default-image'          => get_template_directory_uri() . '/images/literature-city-header.jpg',
+		'default-text-color'     => '1121212',
+		'default-background-color' => 'ffcc00',
+		'width'                  => 1200,
+		'height'                 => 600,
 		'flex-height'            => true,
 		'wp-head-callback'       => 'literature_city_header_style',
 	) ) );
@@ -45,9 +46,21 @@ if ( ! function_exists( 'literature_city_header_style' ) ) :
 			return;
 		}
 
+		/* <img alt="" src="<?php header_image(); ?>" 
+		// width="<?php echo absint( get_custom_header()->width ); ?>" 
+		// height="<?php echo absint( get_custom_header()->height ); ?>">
 		// If we get this far, we have custom styles. Let's do this.
+		*/
 		?>
 		<style type="text/css">
+		<?php
+		if ( has_header_image() ) :
+		?>
+			.site-header {
+				background-image: url("<?php header_image(); ?>");
+			}
+		<?php endif; ?>
+		
 		<?php
 		// Has the text been hidden?
 		if ( ! display_header_text() ) :
